eb485bf79d8e50a5ed49605b906fa8bfd7633eee
taru00 <taru00@gmail.com>
2017-11-26 23:16:09 +0900

    .

---
+++ Source/UE4MMD/UE4MMD.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4MMD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4MMD, "UE4MMD" );
